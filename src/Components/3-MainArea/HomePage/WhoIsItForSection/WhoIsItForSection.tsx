7f33203c8bdfbc5fff92708a4dccc4d5285aa7bf
Alexander Vasilenko <alexandervjr1@gmail.com>
2024-03-02 22:09:44 +0200

    fixed image& section sizing

--- src/Components/3-MainArea/HomePage/WhoIsItForSection/WhoIsItForSection.tsx
+++ src/Components/3-MainArea/HomePage/WhoIsItForSection/WhoIsItForSection.tsx
@@ -1,9 +1,17 @@
 import "./WhoIsItForSection.scss";
-
+import image from '../../../../assets/images/background.png';
 function WhoIsItForSection(): JSX.Element {
     return (
         <div className="WhoIsItForSection">
-            Who is it for section
+            <div className="left">
+                <h1>Who Is This Service For?</h1>
+                <p>
+
+                </p>
+            </div>
+            <div className="right">
+                <img src={image} alt="PBR-logo" />
+            </div>
         </div>
     );
 }
