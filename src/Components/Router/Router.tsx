8ce9a828861e9e5a95ae96584f45b4ab844b07a0
Alexander Vasilenko <alexandervjr1@gmail.com>
2024-03-02 21:28:19 +0200

    .

--- src/Components/Router/Router.tsx
+++ src/Components/Router/Router.tsx
@@ -15,6 +15,7 @@ function Router(): JSX.Element {
 
                 {/* Default Routes */}
                 <Route path="/" element={<Navigate to={appConfig.homeRoute} />} />
+                <Route path="/PBR-Analytics/" element={<Navigate to={appConfig.homeRoute} />} />
                 {/* <Route path="*" element={<PageNotFound />} /> */}
             </Routes>
         </div>
