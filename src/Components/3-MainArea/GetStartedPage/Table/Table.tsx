8ea4e44bfe578122fcbfc10a3a54cf8f0068a3ae
Alexander Vasilenko <alexandervjr1@gmail.com>
2024-03-17 12:22:58 +0200

    added % to table

--- src/Components/3-MainArea/GetStartedPage/Table/Table.tsx
+++ src/Components/3-MainArea/GetStartedPage/Table/Table.tsx
@@ -12,6 +12,7 @@ function Table(s: SettlementModel): JSX.Element {
                     <th>מזהה לשכת מנ"א</th>
                     <th>מועצה אזורית</th>
                     <th>מזהה מועצה אזורית</th>
+                    <th></th>
                     {/* <th className="image-container-cell"><img src={pbrLogo} /></th> */}
                 </tr>
             </thead>
@@ -23,6 +24,7 @@ function Table(s: SettlementModel): JSX.Element {
                     <td>{s.קוד_לשכת_מנא}</td>
                     <td>{s.מועצה_אזורית}</td>
                     <td>{s.קוד_מועצה_אזורית ? s.קוד_מועצה_אזורית : ''}</td>
+                    <td></td>
                 </tr>
             </tbody>
             <thead>
@@ -45,6 +47,28 @@ function Table(s: SettlementModel): JSX.Element {
                     <td>{s.גיל_46_55}</td>
                     <td>{s.גיל_56_64}</td>
                     <td>{s.גיל_65_פלוס}</td>
+                </tr>
+            </tbody>
+            <thead>
+                <tr>
+                    <th></th>
+                    <th>אחוז גילאי 0-5</th>
+                    <th>אחוז גילאי 6-18</th>
+                    <th>אחוז גילאי 19-45</th>
+                    <th>אחוז גילאי 46-55</th>
+                    <th>אחוז גילאי 56-64</th>
+                    <th>אחוז גילאי 65+</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td></td>
+                    <td>{s.סהכ && (s.גיל_0_5 * 100 / s.סהכ).toFixed(1)}{s.סהכ && '%'}</td>
+                    <td>{s.סהכ && (s.גיל_6_18 * 100 / s.סהכ).toFixed(1)}{s.סהכ && '%'}</td>
+                    <td>{s.סהכ && (s.גיל_19_45 * 100 / s.סהכ).toFixed(1)}{s.סהכ && '%'}</td>
+                    <td>{s.סהכ && (s.גיל_46_55 * 100 / s.סהכ).toFixed(1)}{s.סהכ && '%'}</td>
+                    <td>{s.סהכ && (s.גיל_56_64 * 100 / s.סהכ).toFixed(1)}{s.סהכ && '%'}</td>
+                    <td>{s.סהכ && (s.גיל_65_פלוס * 100 / s.סהכ).toFixed(1)}{s.סהכ && '%'}</td>
                 </tr>
             </tbody>
         </table>
