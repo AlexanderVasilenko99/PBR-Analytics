19704f2a4f13e086567c919ddad11e3424957e87
Alexander Vasilenko <alexandervjr1@gmail.com>
2024-03-17 14:18:05 +0200

    maybe

--- src/AppConfig.ts
+++ src/AppConfig.ts
@@ -1,8 +1,8 @@
 class AppConfig {
-    public readonly homeRoute = '/';
-    public readonly contactRoute = '/contact';
-    public readonly aboutRoute = '/about';
-    public readonly getStartedRoute = '/get-started';
+    public readonly homeRoute = 'PBR-Analytics/';
+    public readonly contactRoute = this.homeRoute + 'contact';
+    public readonly aboutRoute = this.homeRoute + 'about';
+    public readonly getStartedRoute = this.homeRoute + 'get-started';
 
     public readonly populationURL = 'https://data.gov.il/api/3/action/datastore_search?resource_id=64edd0ee-3d5d-43ce-8562-c336c24dbc1f';
 }
